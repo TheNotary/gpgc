5f6b08389906fb2ed3f1ee100d28e298147d9efc
TheNotary <no@mail.plz>
2013-03-19 18:06:14 -0500

    sorted out bin file

--- lib/gpgcrypt.rb
+++ lib/gpgcrypt.rb
@@ -25,6 +25,8 @@ module GpgCrypt
     message_string = message
     public_key_string = public_key
     
+    public_key_string = convert_openssh_public_key_to_openssl(public_key_string)
+    
     # encrypt the message
     cipher = Gibberish::RSA.new(public_key_string)
     encrypted_message = cipher.encrypt(message_string)
@@ -45,9 +47,29 @@ module GpgCrypt
     decrypted_message = dec_cipher.decrypt(message_string)
   end
   
+  # This command let's you read your private key and generate from that
+  # a suitable public key which will be posted to pastebin.  
+  def self.export_public_key(private_key)
+    raise "stub: not yet implemented"
+  end
+  
+  def self.help
+    hlp = "gpgcrypt allows you to 'manually' encrypt and decrypt messages over the command line.\n\n"
+    hlp += "To Encrypt something, supply a string to a file containing your message and an openSSL style public key. \n\n"
+    hlp += "To decrypt a message, supply a string to a file containing an encrypted message and a private key.\n\n"
+    hlp += "Soon you'll be able to export a public key from your private key and automatically pastebin it so others can message you.\n\n"
+    hlp += "As an added feature, someday you will be able to specify pastebin links to encrypted messages and public keys"
+  end
+  
   
   def scan_if_encrypted_message(string)
     true
   end
+  
+  def convert_openssh_public_key_to_openssl(public_key_string)
+    public_key_string
+  end
+  
+  
   
 end
