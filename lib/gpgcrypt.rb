a31c3e4b41d58f8a4cf1d13a553c68c086455344
TheNotary <no@mail.plz>
2013-03-19 13:25:32 -0500

    fleshing out algo

--- lib/gpgcrypt.rb
+++ lib/gpgcrypt.rb
@@ -1,5 +1,27 @@
 require "gpgcrypt/version"
 
 module Gpgcrypt
-  # Your code goes here...
+  
+  def encrypt_or_decrypt(*args)
+    code_should_be_encrypted = true
+    
+    scan_if_encrypted_message(args[0])
+    
+    if code_should_be_encrypted
+      encrypt(*args)
+    else
+      decrypt(*args)
+    end
+  end
+  
+  def encrypt(*args)
+    message_string = args[0]
+    public_key = args[1]
+    
+    
+  end
+  
+  def decrypt(*args)
+    
+  end
 end
